29a1f0a6849be5deee0f2ef0474175ab9062488b
Boyd <mdboyd@outlook.com>
2022-05-14 13:02:12 -0400

    Disabled ACLs on resource data sync bucket, enabled bucket keys, fixed customer inventory Lambda permissions

--- AWSSystemsManager/CentralizedPatchManagement/src/lambda-cleanup-patch-ami/cleanup-patch-ami.py
+++ AWSSystemsManager/CentralizedPatchManagement/src/lambda-cleanup-patch-ami/cleanup-patch-ami.py
@@ -1,9 +1,7 @@
 import boto3
 import logging
 from datetime import datetime, timedelta, timezone, tzinfo
-
 logging.basicConfig(level='INFO')
-logging.getLogger('aws_xray_sdk').setLevel(logging.WARNING)
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
@@ -24,34 +22,56 @@ def lambda_handler(event, context):
             }
         ]
     )
-    logging.debug(ami_list)
-    if ami_list.get('Images', None) is None: 
+    
+    log.debug(ami_list)
+    if len(ami_list.get('Images', [])) == 0: 
         log.info("No patching AMIs were returned.")
-        return {}
+        return {}   
     for ami in ami_list['Images']:
         try:
             snapshots = []
-            logging.info(f'AMI: {ami["ImageId"]}')
+            log.info(f'AMI: {ami["ImageId"]}')
             patching_timestamp = [tag['Value'] for tag in ami['Tags'] if tag['Key'] == 'patching-timestamp'][0]
             patching_timestamp = datetime.fromisoformat(patching_timestamp)
             if patching_timestamp.tzinfo is None:
                 patching_timestamp = patching_timestamp.replace(tzinfo=timezone.utc).astimezone()
-            logging.info(f'patching-timestamp: {patching_timestamp}')
+            log.info(f'patching-timestamp: {patching_timestamp}')
             if patching_timestamp < dt_delete:
-                logging.info (f'AMI {ami["ImageId"]} will be de-registered')
+                log.info (f'AMI {ami["ImageId"]} will be de-registered')
                 snapshots = [block_device_mapping.get("Ebs",{}).get("SnapshotId", None) for block_device_mapping in ami["BlockDeviceMappings"]]
-                logging.info(f'Snapshots to delete: {snapshots}')
-                logging.info(f'De-registering AMI {ami["ImageId"]}')
+                log.info(f'Snapshots to delete: {snapshots}')
+                log.info(f'De-registering AMI {ami["ImageId"]}')
                 ec2_client.deregister_image(ImageId=ami["ImageId"])
                 for snapshot in snapshots:
                     try:
                         log.info (f'Deleting AMI snapshot {snapshot}')
                         ec2_client.delete_snapshot(SnapshotId=snapshot)
                     except Exception as ex:
-                        log.error(ex)
-                        log.error(logging.traceback.format_exc())                    
+                        log.exception(ex)                 
         except Exception as ex:        
-            log.error(ex)
-            log.error(logging.traceback.format_exc())
+            log.exception(ex)
+    # Attempt to cleanup any orphaned patching snapshots
+    paginated_snaps = ec2_client.get_paginator('describe_snapshots').paginate(
+        Filters=[
+            {
+                'Name': 'tag-key',
+                'Values': ['patching-timestamp']
+            }
+        ]
+    )
+    for page in paginated_snaps:
+        for snapshot in page['Snapshots']:
+            try: 
+                log.info(snapshot)
+                log.debug(f'Snapshot: {snapshot["SnapshotId"]}')
+                patching_timestamp = [tag['Value'] for tag in snapshot['Tags'] if tag['Key'] == 'patching-timestamp'][0]
+                patching_timestamp = datetime.fromisoformat(patching_timestamp)        
+                if patching_timestamp.tzinfo is None:
+                    patching_timestamp = patching_timestamp.replace(tzinfo=timezone.utc).astimezone()
+                log.info(f'patching-timestamp: {patching_timestamp}')
+                if patching_timestamp < dt_delete:
+                    log.info (f'Deleting AMI snapshot {snapshot["SnapshotId"]}')
+                    ec2_client.delete_snapshot(SnapshotId=snapshot["SnapshotId"])                                        
+            except Exception as ex:        
+                log.exception(ex)
     
-lambda_handler({"retain_days": 4}, {})
